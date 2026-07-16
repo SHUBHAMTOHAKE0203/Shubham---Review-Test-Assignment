e79d9dd50dbecbab7366a80e0b79c5ea09d214be
SHUBHAMTOHAKE0203 <shubhamtohake1@gmail.com>
2026-07-16 14:21:09 +0530

    db connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,21 @@
 import mongoose from 'mongoose';
 
-const connect = async() => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database connected successfully!");
-    } catch(error) {
-        console.error("Database connection error details:", error);
-        process.exit(1);
-    }
+let isConnected = false; 
+
+const connectDB = async () => {
+  if (isConnected) {
+    console.log('=> Using existing database connection');
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = db.connections[0].readyState;
+    console.log('MongoDB Connected Successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
 };
 
-export default connect;
+export default connectDB;
